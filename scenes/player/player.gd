3eaf5c56f3627629c034fade6dff5d2940b49ce0
catonj <catonjb@s.dcsdk12.org>
2026-03-30 15:53:38 -0600

    movement keys

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,8 +8,8 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	#var direction := Input.get_axis("move_left", "move_right")
-	#velocity.x = direction * move_speed
+	var direction := Input.get_axis("move_left", "move_right")
+	velocity.x = direction * move_speed
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
